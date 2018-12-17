ff1cdfaa8274f623c535541014e65f292a140dc3
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 14:31:43 +0100

    Exercice 1 completé

---
+++ Variables - Exercice 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = 1;
+            b = 2;
+            Console.WriteLine("La valeur de A est: " + a);
+            Console.WriteLine("La valeur de B est: " + b);
+
+            c = a + b;
+            Console.WriteLine("La somme de A et B est: " + c);
+        }
+    }
+}
